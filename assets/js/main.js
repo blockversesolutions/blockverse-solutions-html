f53523eb6276dbb67d259c25116ab843eb6796a6
juelthpi <juelcse2019@gmail.com>
2025-01-31 21:56:46 +0600

    desing update

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,8 +137,20 @@ window.addEventListener('load', () => {
   const preloader = document.getElementById('preloader');
   preloader.classList.add('loader-hide');
 });
+ 
+function operations(evt, operations_country) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(operations_country).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-$('.counter').counterUp({
-  delay: 20,
-  time: 1500
-});
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
